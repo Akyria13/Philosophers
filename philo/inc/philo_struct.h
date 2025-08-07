2ebc3a3ce31e948010bc5c27c1cd0d4ba63241b3
Jolan <jolan.wagner@gmail.com>
2025-08-07 19:08:35 +0200

    🔴 Update from school : Update on structure

--- philo/inc/philo_struct.h
+++ philo/inc/philo_struct.h
@@ -6,7 +6,7 @@
 /*   By: jowagner <jowagner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 19:07:46 by jowagner          #+#    #+#             */
-/*   Updated: 2025/07/28 19:11:06 by jowagner         ###   ########.fr       */
+/*   Updated: 2025/08/07 18:59:50 by jowagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,48 @@
 # define PHILO_STRUCT_H
 
 # include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <sys/time.h>
+
+# define PHILO_MAX 200
+
+typedef struct	s_fork
+{
+	int				id;
+	pthread_mutex_t	mutex;
+}				t_fork;
 
 typedef	struct	s_philo
 {
-	pthread_t	*philo;
+	t_fork			*fork_left;
+	t_fork			*fork_right;
+
+	int				id;
+	int				last_meal;
+
+	bool			is_alive;
+	bool			is_eating;
+	bool			is_thinking;
 }				t_philo;
+
+typedef struct	s_data
+{
+	t_philo			*philo;
+
+	t_fork			*fork;
+
+	int				nbr_philo;
+	int				number_of_meal;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				time_to_think;
+
+	pthread_mutex_t	lock_die;
+	pthread_mutex_t	lock_eat;
+	pthread_mutex_t	lock_sleep;
+	pthread_mutex_t	lock_think;
+}				t_data;
 
 #endif
